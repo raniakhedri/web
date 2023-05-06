3b8f6defa58cf940a19b18cd8fac6f1acf01b695
Rania khedri <rania.khedri@esprit.tn>
2023-05-06 12:21:24 +0100

    Update gestionreclamation.php

--- VIEWS/back/gestionreclamation.php
+++ VIEWS/back/gestionreclamation.php
@@ -73,14 +73,37 @@ if (isset($_GET['unarchive'])) {
     <div class="min-h-screen flex flex-col">
         <!--Header Section Starts Here-->
         <header class="bg-nav">
-            <div class="flex justify-between">
-                <div class="p-1 mx-3 inline-flex items-center">
-                    <i class="fas fa-bars pr-2 text-white" onclick="sidebarToggle()"></i>
-                    <h1 class="text-white p-2">DisTunis</h1>
+                <div class="flex justify-between">
+                    <div class="p-1 mx-3 inline-flex items-center">
+                        <i class="fas fa-bars pr-2 text-white" onclick="sidebarToggle()"></i>
+                        <img src="./logo.png" class="logok" width="200px">
+                    </div>
+                    <div class="p-1 flex flex-row items-center">
+                        <a href="https://github.com/tailwindadmin/admin"
+                            class="text-white p-2 mr-2 no-underline hidden md:block lg:block"></a>
+
+
+
+                        <a href="#" onclick="" class="text-white p-2 no-underline hidden md:block lg:block"></a>
+                        <div id="ProfileDropDown"
+                            class="rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
+                            <ul class="list-reset">
+                                <li><a href="#" class="no-underline px-4 py-2 block text-black hover:bg-grey-light">My
+                                        account</a></li>
+                                <li><a href="#"
+                                        class="no-underline px-4 py-2 block text-black hover:bg-grey-light">Notifications</a>
+                                </li>
+                                <li>
+                                    <hr class="border-t mx-2 border-grey-ligght">
+                                </li>
+                                <li><a href="#"
+                                        class="no-underline px-4 py-2 block text-black hover:bg-grey-light">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-               
-            </div>
-        </header>
+            </header>
         <!--/Header-->
 
         <div class="flex flex-1">
